ce0078c97161d19515ccb7b629e453ddd8c4927d
jonariv09 <jonariv09@gmail.com>
2020-05-29 17:58:41 -0600

    BusinessPreview component and CreateStore page, finished

--- src/components/ BusinessPreview/index.js
+++ src/components/ BusinessPreview/index.js
@@ -5,8 +5,10 @@ import {
   FiPenTool,
   FiImage,
   FiClock,
-  FiStar
+  FiStar,
+  FiSearch
 } from "react-icons/fi";
+import StorePicture from '../../assets/StorePicture.png'
 import "./index.css";
 import "./overview.css";
 
@@ -14,16 +16,24 @@ export default class BusinessPreview extends Component {
   render() {
     return (
       <>
-        <div className="card">
+        <div className="card card-business-preview my-3">
+          <p className="font-weight-bold"> Vista previa de tu negocio </p>
           <div className="card-header">
-            <p> Vista previa de tu negocio </p>
-            <img src={BannerPreview} alt="" width="100%" />
-            <div>
-              <span class="img-thumbnail"> </span>
-              <p> Nombre de tu negocio </p>
-              <p> Categoría </p>
+            <img
+              src={BannerPreview}
+              className="banner-business-preview"
+              alt=""
+              width="100%"
+            />
+            <div className="px-3 d-flex align-items-end business-name-container">
+              <img src={StorePicture} class="img-thumbnail" alt="" />
+              <div className="ml-3 mb-4">
+                <p className="m-0 font-weight-bold"> Nombre de tu negocio </p>
+                <p className="m-0 font-weight-light"> Categoría </p>
+              </div>
             </div>
-            <nav class="navbar navbar-expand-lg">
+
+            <nav class="navbar-business-preview navbar navbar-expand-lg p-0">
               <ul className="navbar-nav">
                 <li class="nav-item active">
                   <a class="nav-link" href="#">
@@ -65,16 +75,24 @@ export default class BusinessPreview extends Component {
                     </a>
                   </div>
                 </li>
+              </ul>
 
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">
+              <ul className="navbar-nav ml-3">
+                <li class="nav-item icon-item-shadow active">
+                  <a class="nav-link icon-link-shadow" href="#">
+                    <FiSearch />
+                  </a>
+                </li>
+
+                <li class="nav-item icon-item-shadow active">
+                  <a class="nav-link icon-link-shadow" href="#">
                     <FiMoreHorizontal />
                   </a>
                 </li>
 
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">
-                    <FiPenTool />
+                  <a class="nav-link icon-link" href="#">
+                    <FiPenTool className="mr-1" />
                     Editar perfil
                   </a>
                 </li>
@@ -83,32 +101,36 @@ export default class BusinessPreview extends Component {
           </div>
 
           <div className="card-body">
-            <div className="row">
-              <div className="description-section">
+            <div className="">
+              <div className="description-section px-4 py-1 mb-2">
+                <p className="m-0"> Descripción </p>
                 <div className="skeleton-description-line"></div>
                 <div className="skeleton-description-line"></div>
                 <div className="skeleton-description-line"></div>
+                <p className="text-right m-0"> Editar detalles </p>
               </div>
 
-              <div className="announcement-section">
-                <div class="form-group">
+              <div className="announcement-section px-3 py-2">
+                <div class="form-group m-1">
                   <input
                     type="text"
-                    class="form-control"
+                    class="form-control announcement-input"
                     id="announcement"
                     name="announcement"
+                    placeholder="Anuncio"
                   />
                 </div>
+                <div className="horizontal-divider"></div>
 
-                <div className="icons-container">
-                  <div className="item-icons-container">
-                    <FiImage /> Nueva foto
+                <div className="icons-container pt-2">
+                  <div className="item-icons-container mx-2 d-flex align-items-center">
+                    <FiImage className="icon-announcement mr-1"/> Nueva foto
                   </div>
-                  <div className="item-icons-container">
-                    <FiClock /> Próximamente
+                  <div className="item-icons-container mx-2 d-flex align-items-center">
+                    <FiClock className="icon-announcement mr-1"/> Próximamente
                   </div>
-                  <div className="item-icons-container">
-                    <FiStar /> Producto estrella
+                  <div className="item-icons-container mx-2 d-flex align-items-center">
+                    <FiStar className="icon-announcement mr-1"/> Producto estrella
                   </div>
                 </div>
               </div>
