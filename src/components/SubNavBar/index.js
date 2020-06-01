ef4c9db9f00ec1cace0a61d0082e9de74ab42489
jonariv09 <jonariv09@gmail.com>
2020-06-01 02:01:06 -0600

    Register modal form created

--- src/components/SubNavBar/index.js
+++ src/components/SubNavBar/index.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import { FcShop } from "react-icons/fc";
 import "./index.css";
+import "./overwrite.css";
+// import Facebook from "../../assets/images/facebook.svg";
+// import Twitter from "../../assets/images/twitter.svg";
+// import Google from "../../assets/images/google.svg";
+
+import {FaFacebookF, FaTwitter, FaGoogle} from "react-icons/fa";
 
 export default class SubNavBar extends Component {
   render() {
@@ -32,30 +38,68 @@ export default class SubNavBar extends Component {
             >
               <div class="modal-dialog">
                 <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">
-                      Modal title
-                    </h5>
-                    <button
-                      type="button"
-                      class="close"
-                      data-dismiss="modal"
-                      aria-label="Close"
-                    >
-                      <span aria-hidden="true">&times;</span>
-                    </button>
+                  <div class="modal-header d-flex flex-column align-items-center pb-0">
+                    <span className="py-2 lead font-weight-bold text-dark"> Registrar con </span>
+                    <span className="py-2 text-muted "> Registrate con tu cuenta de redes </span>
+
+                    <div className="d-flex justify-content-center">
+                      <a href="/404" className="icon-social-network-modal icon-facebook d-flex justify-content-center align-items-center mx-2">
+                        <FaFacebookF className="text-center" />
+                      </a>
+                      <a href="/404" className="icon-social-network-modal icon-twitter d-flex justify-content-center align-items-center mx-2">
+                        <FaTwitter className="text-center" />
+                      </a>
+                      <a href="/404" className="icon-social-network-modal icon-google d-flex justify-content-center align-items-center mx-2">
+                        <FaGoogle className="text-center" />
+                      </a>
+                    </div>
+
+                    <div className="d-flex justify-content-center align-items-center w-100 mt-3">
+                      <div className="divider-modal"></div>
+                      <div className="circle"></div>
+                      <div className="divider-modal"></div>
+                    </div>
+
                   </div>
-                  <div class="modal-body">...</div>
-                  <div class="modal-footer">
-                    <button
-                      type="button"
-                      class="btn btn-secondary"
-                      data-dismiss="modal"
-                    >
-                      Close
-                    </button>
+                  <div class="modal-body py-2">
+                    <form method="post">
+                      <div class="form-group">
+                        <input
+                          type="email"
+                          class="form-control"
+                          id="exampleInputEmail1"
+                          placeholder="Nombre de usuario"
+                        />
+                      </div>
+                      <div class="form-group">
+                        <input
+                          type="email"
+                          class="form-control"
+                          id="exampleInputEmail1"
+                          placeholder="Correo"
+                        />
+                      </div>
+                      <div class="form-group">
+                        <input
+                          type="email"
+                          class="form-control"
+                          id="exampleInputEmail1"
+                          placeholder="Contraseña"
+                        />
+                      </div>
+                      <div class="form-group">
+                        <input
+                          type="email"
+                          class="form-control"
+                          id="exampleInputEmail1"
+                          placeholder="Escriba de nuevo su contraseña"
+                        />
+                      </div>
+                    </form>
+                  </div>
+                  <div class="modal-footer d-flex justify-content-center pt-0">
                     <button type="button" class="btn btn-primary">
-                      Save changes
+                      Registrarse
                     </button>
                   </div>
                 </div>
