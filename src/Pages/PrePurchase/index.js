e8fade4708aa8b819003d1acee59fbba9af9a7b6
jonariv09 <jonariv09@gmail.com>
2020-07-10 04:16:31 -0600

    Page pre purchased created

--- src/Pages/PrePurchase/index.js
+++ src/Pages/PrePurchase/index.js
@@ -3,20 +3,53 @@ import NavBar from '../../components/NavBar'
 
 import computadoraGamer from '../../assets/images/computadoraGamer.jpg'
 
+import {FiStar, FiMinusSquare, FiPlusSquare} from 'react-icons/fi'
+
 import './index.scss'
 import './overwrite.scss'
 
 export default class PrePurchase extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {quantity: 1, priceAdded: 20000, productPrice: 20000}
+
+    this.handleMinusQuantity = this.handleMinusQuantity.bind(this)
+    this.handleMoreQuantity = this.handleMoreQuantity.bind(this)
+  }
+
+  handleMinusQuantity() {
+    if (this.state.quantity > 1) {
+      this.setState({
+        quantity: this.state.quantity - 1,
+      })
+
+      this.setState({
+        priceAdded: this.state.priceAdded - this.state.productPrice,
+      })
+    }
+  }
+
+  handleMoreQuantity() {
+    this.setState({
+      quantity: this.state.quantity + 1,
+    })
+
+    this.setState({
+      priceAdded: this.state.priceAdded + this.state.productPrice,
+    })
+  }
+
   render() {
     return (
       <>
         <NavBar />
 
-        <div className="container">
+        <div className="container pre-purchase">
           <div className="row mt-5">
             <div className="col-6">
               <div className="d-flex flex-column align-items-center">
-                <img src={computadoraGamer} className="big-image" />
+                <img src={computadoraGamer} className="big-image" alt="" />
               </div>
               <div className="small-images">
                 <img src={computadoraGamer} className="small-image" alt="" />
@@ -36,7 +69,60 @@ export default class PrePurchase extends Component {
                 bibendum neque.
               </p>
 
-              <div className="ranking"></div>
+              <div className="ranking-container my-4">
+                <FiStar className="ranking-star" />
+                <FiStar className="ranking-star" />
+                <FiStar className="ranking-star" />
+                <FiStar className="ranking-star" />
+                <FiStar className="ranking-star" />
+              </div>
+
+              <div>
+                <p>
+                  <span className="pr-3 text-muted"> Fabricante: </span> NOC
+                </p>
+                <p>
+                  <span className="pr-3 text-muted"> Disponibilidad: </span> 5
+                  en stand
+                </p>
+                <p>
+                  <span className="pr-3 text-muted"> Envio: </span> C$ 25
+                </p>
+
+                <p className="font-weight-bold">
+                  <span className="pr-3 text-muted"> Precio: </span> C${' '}
+                  {this.state.priceAdded}
+                </p>
+
+                <div className="quantity-container">
+                  <span className="text-muted"> Cantidad: </span>
+
+                  <button
+                    className="quantity-less-button btn"
+                    onClick={this.handleMinusQuantity}
+                  >
+                    <FiMinusSquare />
+                  </button>
+
+                  <span className="quantity-number">{this.state.quantity}</span>
+
+                  <button
+                    className="quantity-more-button btn"
+                    onClick={this.handleMoreQuantity}
+                  >
+                    <FiPlusSquare />
+                  </button>
+                </div>
+              </div>
+
+              <div className="buttons-container mt-4 mb-5">
+                <button type="button" className="btn btn-buy">
+                  Comprar
+                </button>
+                <button type="button" className="btn btn-add-car">
+                  Agregar al Carro
+                </button>
+              </div>
             </div>
           </div>
         </div>
