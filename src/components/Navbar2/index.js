81eaabb17fd589554d8db33132690784fdc30018
haywill20 <mirandacaceresm6@gmail.com>
2020-07-20 12:43:17 -0600

    NavBar2 responsive
    modifique todo el codigo de NabBar2, ya esta responsive

--- src/components/Navbar2/index.js
+++ src/components/Navbar2/index.js
@@ -1,108 +1,146 @@
 import React, {Component} from 'react'
 import './index.scss'
 import Logo from '../../assets/images/logo.png'
-import Traducir from '../../assets/images/traducir.png'
-import EnglishImage from '../../assets/images/english_language.png'
-
-import {AiOutlineShoppingCart, AiOutlineSearch} from 'react-icons/ai'
+import {FiShoppingCart, FiSearch, FiUser} from 'react-icons/fi'
+import {MdLanguage} from 'react-icons/md'
+import {AiOutlineSearch} from 'react-icons/ai'
 
 const styles = {
   iconSize: 21,
+}
+const icons = {
+  size: 25,
 }
 
 export default class Navbar2 extends Component {
   render() {
     return (
       <>
-        <nav className="navbar navbar-expand-lg main-navbar flex-row">
-          <div className="row py-1">
-            {/*Logo*/}
-            <div className="col-xl-3 d-flex align-items-center">
-              <a href="/">
-                <img src={Logo} className="img-logo" alt="" />
-              </a>
-            </div>
-
-            {/*Buscador*/}
-            <div className="col-xl-4 ml-auto d-flex align-items-center">
-              <div className="input-group input-group-sm">
-                <input
-                  type="text"
-                  className="form-control form-control-main-search"
-                  aria-label="Sizing example input"
-                  placeholder="Buscar"
-                />
-                <div className="input-group-append search-group-append">
-                  <a
-                    href="#"
-                    className="input-group-text search-text-group"
-                    id="inputGroup-sizing-sm"
-                  >
-                    <AiOutlineSearch
-                      className="icon-main-search"
-                      size={styles.iconSize}
-                    />
-                  </a>
-                </div>
-              </div>
-
-              <div className="dropdown ml-4">
-                <img
-                  src={Traducir}
-                  alt="..."
-                  className="language-icon mr-5 dropdown-toggle language-dropdown"
+        <nav className="navbar navbar-expand-lg navbar-light bg-light row">
+          {/*****************************************************************************/}
+          {/*****************************************************************************/}
+          {/*Logo*/}
+          <div className="col-3">
+            <a className="logo navbar-brand mr-auto " href="#">
+              <img src={Logo} className="image-logo" alt="" />
+            </a>
+          </div>
+          {/*Fin Logo*/}
+          {/*****************************************************************************/}
+          {/*****************************************************************************/}
+          {/*Iconos en tamaño responsive*/}
+          <div className="responsive-navbar navbar-toggler">
+            <FiSearch size={icons.size} className="responsive-navbar-item" />
+            <FiShoppingCart
+              size={icons.size}
+              className="responsive-navbar-item"
+            />
+            <FiUser size={icons.size} className="responsive-navbar-item" />
+          </div>
+          {/*Fin Iconos en tamaño responsive*/}
+          {/*****************************************************************************/}
+          {/*****************************************************************************/}
+          {/*Boton desplegable en tamaño responsive*/}
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          {/*Fin Boton desplegable en tamaño responsive*/}
+          {/*****************************************************************************/}
+          {/*****************************************************************************/}
+          {/* inicio Buscador*/}
+          <div className="col-xl-4 ml-auto d-flex align-items-center">
+            <div className="input-group input-group-sm">
+              <input
+                type="text"
+                className="form-control form-control-main-search"
+                aria-label="Sizing example input"
+                placeholder="Buscar"
+              />
+              <div className="input-group-append search-group-append">
+                <a
                   href="#"
-                  id="dropdownMenuButton"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                />
-
-                <div
-                  className="dropdown-menu"
-                  aria-labelledby="dropdownMenuButton"
+                  className="input-group-text search-text-group"
+                  id="inputGroup-sizing-sm"
                 >
-                  <a
-                    className="dropdown-item d-flex align-items-center"
-                    href="#"
-                  >
-                    <img
-                      src={EnglishImage}
-                      alt="..."
-                      className="rounded-circle language-icon mr-2"
-                    />
-                    English
-                  </a>
-                </div>
+                  <AiOutlineSearch
+                    className="icon-main-search"
+                    size={styles.iconSize}
+                  />
+                </a>
               </div>
             </div>
-            <button type="button" className="btn btn-success btn-sm ">
-              Iniciar sesión
-            </button>
-            <div className="col-xl-4 ml-auto d-flex align-items-center">
-              <div className="ml-auto d-flex justify-content-center">
-                <a href="/404" className="item icon-link-navbar">
-                  Soluciones
-                </a>
-                <div className="mr-auto divider"> </div>
+          </div>
+          {/*Fin inicio Buscador*/}
+          {/*****************************************************************************/}
+          {/*****************************************************************************/}
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            {/*****************************************************************************/}
+            {/*****************************************************************************/}
+            {/*Idioma*/}
 
-                <a href="/404" className="item icon-link-navbar">
-                  Porque elegirnos
+            <div className="dropdown col-1 ">
+              <MdLanguage
+                className="language-icon mr-5 dropdown-toggle language-dropdown "
+                href="#"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              />
+              <div
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton"
+              >
+                <a className="dropdown-item d-flex align-items-center" href="#">
+                  Español
                 </a>
-                <div className="mr-auto divider"></div>
-
-                <div className="item position-relative">
-                  <a href="/shopping-car" className="icon-link-navbar">
-                    <AiOutlineShoppingCart
-                      size={styles.iconSize}
-                      className="icon-item mr-2"
-                    />
-                    Carro
-                  </a>
-                  <span className="badge badge-shopping-cart"> 3 </span>
-                </div>
               </div>
             </div>
+            <div className="col-4 d-none d-md-block">
+              <button className="btn btn-sm btn-success" title="Iniciar sesión">
+                Iniciar sesión
+              </button>
+            </div>
+
+            {/*Fin Idioma*/}
+            {/*****************************************************************************/}
+            {/*****************************************************************************/}
+            {/*Inicio Opciones*/}
+
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">
+                  Soluciones <span class="sr-only"> (current) </span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Porque Elegirnos
+                </a>
+              </li>
+
+              <li class="nav-item d-none d-md-block ">
+                <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">
+                  <FiShoppingCart
+                    size={icons.size}
+                    className="responsive-navbar-item"
+                  />
+                  Carro
+                </a>
+              </li>
+            </ul>
+
+            {/*Fin Opciones*/}
+            {/*****************************************************************************/}
+            {/*****************************************************************************/}
           </div>
         </nav>
       </>
